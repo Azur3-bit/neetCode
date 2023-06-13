3aeb946e8abd9bc9efd692fb9357a4923c31aeb0
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-06-13 19:08:59 +0530

    Group AnaGrams Try - 1(batch-2)

--- Group anagrams Try -2 .cpp
+++ Group anagrams Try -2 .cpp
@@ -28,8 +28,26 @@ void showVector(vector<string> temp)
     {
         cout << temp[i] << endl;
     }
-    cout << "\t\t::Vector has been Printed :: "
+    cout << "\t\t:: Vector has been Printed :: "
          << "\n";
+}
+
+
+
+
+vector<string> SortingFunc(vector<string> &vecString){
+    sort(vecString.begin(), vecString.end());
+    return vecString;
+}
+
+
+void Solution(vector<string> vecString){
+
+    map<string, string> hashMap;
+    int size = vecString.size();
+    // for (int i = 0; i < size;i++){
+    //     hashMap[vecString[i]] = 
+    // }
 }
 
 int main(int argc, char const *argv[])
@@ -39,6 +57,11 @@ int main(int argc, char const *argv[])
     vector<string> strVec = {"eat", "tea", "tan", "ate", "nat", "bat"};
 
     showVector(strVec);
+
+    
+    // Sorting Vector
+    // SortingFunc(strVec);
+    // showVector(strVec);
 
     return 0;
 }
