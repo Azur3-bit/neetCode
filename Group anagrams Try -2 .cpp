3aa6a04c90152dab2520036bfaed14ad6e835386
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-06-15 13:02:03 +0530

    appr - 2 (Group Anagrams)

--- Group anagrams Try -2 .cpp
+++ Group anagrams Try -2 .cpp
@@ -32,22 +32,27 @@ void showVector(vector<string> temp)
          << "\n";
 }
 
-
-
-
-vector<string> SortingFunc(vector<string> &vecString){
+vector<string> SortingFunc(vector<string> &vecString)
+{
     sort(vecString.begin(), vecString.end());
     return vecString;
 }
 
+void Solution(vector<string> vecString)
+{
 
-void Solution(vector<string> vecString){
-
-    map<string, string> hashMap;
+    map<string, int> hashMap;
     int size = vecString.size();
-    // for (int i = 0; i < size;i++){
-    //     hashMap[vecString[i]] = 
-    // }
+    for (int i = 0; i < size; i++)
+    {
+        int TotalSizeOfWord = 0;
+        string CurrentWord = vecString[i];
+        int CurrentSize = CurrentWord.size();
+        // for (int j = 0; j < CurrentSize;j++){
+
+        //     hashMap[vecString[i]] =
+        // }
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -58,7 +63,16 @@ int main(int argc, char const *argv[])
 
     showVector(strVec);
 
-    
+    vector<string> strTest = {"aaa", "aa", "a"};
+    string str_1 = strTest[0];
+    string str_2 = strTest[1];
+
+    int Element_1Size = sizeof(str_1);
+    int Element_2Size = sizeof(str_2);
+    cout << str_1 << " ELement_1size " << Element_1Size << endl;
+    cout << str_2 << " ELement_2size " << Element_2Size << endl;
+    // cout << "word_2size" << word_1size;
+
     // Sorting Vector
     // SortingFunc(strVec);
     // showVector(strVec);
