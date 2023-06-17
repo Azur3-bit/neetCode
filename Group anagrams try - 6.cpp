ad82f9df61806bda7fe8c6b70bf0a8a3d89d531d
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-06-17 20:27:48 +0530

    Sorting try -6

--- Group anagrams try - 6.cpp
+++ Group anagrams try - 6.cpp
@@ -1,11 +1,10 @@
 // Group anagrams try - 6
 
-// Sorting String Vector Approach 
+// Sorting String Vector Approach
 
 // Problem Description
 // Input: strs = ["eat","tea","tan","ate","nat","bat"]
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-
 
 // -- essential_libraries_and_namespace
 #include <iostream>
@@ -19,23 +18,34 @@ using namespace std;
 #include <map>
 #include <set>
 
-    // Classes
-    
-    // Functions
+// Classes
 
+// Functions
+
+void ShowVector(vector<string> strs){
+    for(auto it : strs){
+        cout << it << " ";
+    }
+}
+
+vector<string> solution(vector<string> &strs){
+    sort(strs.begin(), strs.end());
+    // ShowVector(strs);
+    return strs;
+}
 
 int main(int argc, char const *argv[])
 {
 
-    // -- faster I/O operations 
+    // -- faster I/O operations
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     // -- Main Code Function
 
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    solution(strs);
+    ShowVector(strs);
 
     return 0;
 }
-
-
-
