dc2fe371b283d5d233d399a07d0c63922d0b2d74
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-06-20 03:19:07 +0530

    Priortiy Queues Start

--- Learning Heap.cpp
+++ Learning Heap.cpp
@@ -1,1 +1,45 @@
-Learning Heap 
+// ?        -- Learning Heap --
+
+// *           Love babbar --- https://www.youtube.com/watch?v=NKJnHewiGdc
+
+// TODO -- Checklist --
+// What are Heaps
+// Max heap
+// min heap
+// insertion
+// deletion
+// Heapify Algorithm
+// Heap Sort
+// Priority Queue
+// Priority Queue - Min Heap
+// Priority Queue - Max Heap
+
+// -- essential_libraries_and_namespace
+#include <iostream>
+using namespace std;
+
+// -- additional_libraries
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <map>
+#include <set>
+// -- additional_libraries --azure
+#include "TimerClock.h"
+
+// Classes
+
+// Functions
+
+int main(int argc, char const *argv[])
+{
+
+    // -- faster I/O operations
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // -- Main Code Function
+
+    return 0;
+}
