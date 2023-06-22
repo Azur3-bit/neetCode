76d15e1a9711bd3d2373313bb4a1109bc009ab37
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-06-23 00:28:38 +0530

    using STL-Heaps

---
+++ using STL - Heap.cpp
@@ -0,0 +1,54 @@
+// using Stl for Heap - priority Queue
+
+// -- essential_libraries_and_namespace
+#include <iostream>
+using namespace std;
+
+// -- additional_libraries
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <queue>
+// -- additional_libraries --azure
+#include "TimerClock.h"
+
+// Classes
+
+// Functions
+
+int main(int argc, char const *argv[])
+{
+
+    // -- faster I/O operations
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // -- Main Code Function
+
+    priority_queue<int> pq;
+    pq.push(90);
+    pq.push(100);
+    pq.push(110);
+    pq.push(1);
+    pq.push(10);
+
+    // printing the priority Queue
+
+    cout << "Element on Top " << pq.top() << "\n";
+
+    // Printing whole Priortiy Queue
+
+    cout << "\t\t --:-: Printing Whole Priority Queue :-:-- "
+         << "\n";
+
+    while (pq.size())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    
+    return 0;
+}
