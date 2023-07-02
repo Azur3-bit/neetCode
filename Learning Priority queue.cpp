5561075c8b715621ae28711ff93c51d844ab215c
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-03 04:54:14 +0530

    completed Top k Frequent Elements

--- Learning Priority queue.cpp
+++ Learning Priority queue.cpp
@@ -19,22 +19,45 @@ using namespace std;
 
 // Functions
 
-void _showPriortiyQueue(priority_queue<int> &pq){
+void _showPriortiyQueue(priority_queue<int> &pq)
+{
     while (!pq.empty())
     {
         cout << pq.top() << " ";
         pq.pop();
     }
-    
 }
 
 void priorityQueue(const vector<int> &nums)
 {
     priority_queue<int> pq;
-    for (int it : nums){
+    for (int it : nums)
+    {
         pq.push(it);
     }
     _showPriortiyQueue(pq);
+}
+
+void _showVectorPair(priority_queue<pair<int, int>, vector<pair<int, int>>> &pqm)
+{
+    cout << "Showing Priortiy Vector Pair \n";
+    cout << "VecPriortiy.first \t\t VecPriortiy.second\n";
+    while (!pqm.empty())
+    {
+        pair<int, int> TopELement = pqm.top();
+        cout << TopELement.first << "\t\t" << TopELement.second << "\n";
+        pqm.pop();
+    }
+}
+
+void pair_priortityQueue(unordered_map<int, int> &mps)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>> pq{};
+    for (auto it : mps)
+    {
+        pq.push(it);
+    }
+    _showVectorPair(pq);
 }
 
 int main(int argc, char const *argv[])
@@ -45,7 +68,10 @@ int main(int argc, char const *argv[])
     cin.tie(NULL);
 
     // -- Main Code Function
-    vector<int> nums = {100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    priorityQueue(nums);
+    // vector<int> nums = {100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    unordered_map<int, int> ump = {{12, 1}, {13, 2}, {15, 7}};
+    // priorityQueue(nums);
+    pair_priortityQueue(ump);
+
     return 0;
 }
--- TopK_frequent Elements.cpp
+++ TopK_frequent Elements.cpp
@@ -27,15 +27,33 @@ using namespace std;
 
 void _showMap(const unordered_map<int, int> &nums)
 {
-    cout << "Key"
-         << "\t\t"
-         << "Value\n";
+    cout << "Key \t\t Value\n";
     for (auto it : nums)
     {
         cout << it.first << "\t\t" << it.second << "\n";
     }
 }
 
+void _showVectorPairQueue(priority_queue<pair<int, int>, vector<pair<int, int>>> &pmq)
+{
+    cout << "Pmq.first \t\t Pmq.second\n";
+    // cout << "Size : " << pmq.size();
+    while (!pmq.empty())
+    {
+        pair<int, int> curr_element = pmq.top();
+        cout << curr_element.first << "\t\t" << curr_element.second << "\n";
+        pmq.pop();
+    }
+}
+
+void _showVector(const vector<int> &nums)
+{
+    cout << "result \n";
+    for (int it : nums)
+    {
+        cout << it << " ";
+    }
+}
 vector<int> TopKFreqElements(const vector<int> &nums, int k)
 {
     vector<int> result{};
@@ -47,11 +65,29 @@ vector<int> TopKFreqElements(const vector<int> &nums, int k)
         int curr_count = count(nums.begin(), nums.end(), it);
         mp[it] = curr_count;
     }
+    // _showMap(mp);
+
     // Pushing it into a priority Queue - Min Heap
+    priority_queue<pair<int, int>, vector<pair<int, int>>> pqm{};
+    for (auto it : mp)
+    {
+        // Swaping First and Second
+        int first = it.first;
+        int second = it.second;
+        pair<int, int> swaped_FisrtSecond = pair<int, int>(second, first);
+        pqm.push(swaped_FisrtSecond);
+    }
 
-    _showMap(mp);
+    // _showVectorPairQueue(pqm);
 
-    
+    // Getting Top K elements into the Priortiy Queue
+    while (k--)
+    {
+        pair<int, int> TopElement;
+        TopElement = pqm.top();
+        result.push_back(TopElement.second);
+        pqm.pop();
+    }
 
     return result;
 }
@@ -68,7 +104,8 @@ int main(int argc, char const *argv[])
     vector<int> nums = {1, 1, 1, 2, 2, 3};
     int k = 2;
 
-    TopKFreqElements(nums, k);
+    vector<int> result = TopKFreqElements(nums, k);
+    _showVector(result);
 
     return 0;
 }
