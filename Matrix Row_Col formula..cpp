8d945f128f57f4aee2c24e991e54f16fe33147f5
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-18 23:38:35 +0530

    Created Div Selector - Suduko Checker

--- Matrix Row_Col formula..cpp
+++ Matrix Row_Col formula..cpp
@@ -11,23 +11,69 @@ using namespace std;
 #include <map>
 #include <set>
 #include <queue>
-// -- additional_libraries -- azur3 
+// -- additional_libraries -- azur3
 #include "TimerClock.h"
 
-    // Classes
-    
-    // Functions
+// Classes
 
+// Functions
+
+// -- traverse nested vector
+void _traverseNestedVector(vector<vector<int>> &traVec)
+{
+    cout << "\n ::Printing Nested Vector :: \n ";
+    for (vector<int> it : traVec)
+    {
+        cout << "{ ";
+        for (int i : it)
+        {
+            cout << i << " ";
+        }
+        cout << "}";
+        cout << endl;
+    }
+}
+
+// -- Matrix Formula
+void _matrixRowColGenerator(vector<vector<int>> &vec)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            int k = (((i / 3) * 3) + (j / 3)) ;
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
 
-    // -- faster I/O operations 
+    // -- faster I/O operations
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     // -- Main Function code --
 
+    // cout << "Working";
 
+vector<vector<int>> usr_input = {
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {10, 20, 30, 40, 50, 60, 70, 80, 90},
+    {11, 12, 13, 14, 15, 16, 17, 18, 19},
+
+    {101, 201, 301, 401, 501, 601, 701, 801, 901},
+    {102, 202, 302, 402, 502, 602, 702, 802, 902},
+    {103, 203, 303, 403, 503, 603, 703, 803, 903},
+
+    {100, 200, 300, 400, 500, 600, 700, 800, 900},
+    {110, 210, 310, 41, 510, 610, 710, 810, 910},
+    {1001, 2001, 3001, 4001, 5001, 6001, 7001, 8001, 9001},
+};
+
+    _traverseNestedVector(usr_input);
+    _matrixRowColGenerator(usr_input);
     return 0;
 }
--- Sudoko Checker.cpp
+++ Sudoko Checker.cpp
@@ -22,6 +22,7 @@ void _traverseNestedVector(vector<vector<int>> &temp)
 {
     cout << "\n:: Printing Nested Vector :: \n";
     cout << "{ ";
+    cout << endl;
     for (vector<int> tempItr : temp)
     {
         cout << "{ ";
@@ -30,8 +31,9 @@ void _traverseNestedVector(vector<vector<int>> &temp)
             cout << it << " ";
         }
         cout << "} ";
+        cout << endl;
     }
-    cout << "} ";
+    cout << "} \n";
 }
 
 // -- Traversing Vector
@@ -61,12 +63,12 @@ void _traverseSet(set<int> &usr_set)
 bool _checkingRow(vector<vector<int>> usr_input)
 {
     cout << "\nChecking Row\n";
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 9; i++)
     {
         set<int> rowSet{};
         int counter = 0;
         vector<int> curr_vector = usr_input[i];
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 9; j++)
         {
             if (curr_vector[j] >= 0 && curr_vector[j] < 10)
             {
@@ -84,13 +86,13 @@ bool _checkingCol(vector<vector<int>> &usr_input)
 {
     cout << "\nChecking Column \n";
 
-    cout << "\n printing Matrix Column-Wise \n";
+    // cout << "\n printing Matrix Column-Wise \n";
     set<int> temp_set;
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 9; i++)
     {
         temp_set = {};
         int counter = 0;
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 9; j++)
         {
             int curr_element = usr_input[j][i];
             if (curr_element >= 0 && curr_element < 10)
@@ -105,6 +107,50 @@ bool _checkingCol(vector<vector<int>> &usr_input)
     }
 }
 
+void _matrixRowColGenerator(vector<vector<int>> &vec)
+{
+    vector<vector<int>> divEleCollection = {
+        {}, // --> 1
+        {}, // --> 2
+        {}, // --> 3
+        {}, // --> 4
+        {}, // --> 5
+        {}, // --> 6
+        {}, // --> 7
+        {}, // --> 8
+        {}, // --> 9
+    };
+
+    int k = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            k = (((i / 3) * 3) + (j / 3));
+            cout << " Pushing : " << vec[i][j] << " in : " << k << "\n";
+            divEleCollection[k].push_back(vec[i][j]);
+        }
+        cout << "\n\t-- -- --- -- -- -- ";
+
+        cout << "\n\t-- Printing Kth Part of Vector -- ";
+        _traverseVector(divEleCollection[k]);
+
+        cout << endl;
+    }
+}
+
+// * Done -- Verified after using this function
+void TestFunction()
+{
+    vector<vector<int>> divEleCollection = {{}, {}, {}};
+    divEleCollection[0].push_back(0);
+    divEleCollection[1].push_back(1);
+    divEleCollection[2].push_back(2);
+
+    // Traversing divEleCollection
+    _traverseNestedVector(divEleCollection);
+}
+
 int main()
 {
 
@@ -114,9 +160,26 @@ int main()
 
     // -- Main Function code --
 
-    vector<vector<int>> usr_input = {{1, 2, 3}, {1, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> usr_input = {
+        // <-- ROW - 1 -->
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {10, 20, 30, 40, 50, 60, 70, 80, 90},
+        {11, 12, 13, 14, 15, 16, 17, 18, 19},
+        // <-- ROW - 2 -->
+        {101, 201, 301, 401, 501, 601, 701, 801, 901},
+        {102, 202, 302, 402, 502, 602, 702, 802, 902},
+        {103, 203, 303, 403, 503, 603, 703, 803, 903},
+        // <-- ROW - 3 -->
+        {100, 200, 300, 400, 500, 600, 700, 800, 900},
+        {110, 210, 310, 41, 510, 610, 710, 810, 910},
+        {1001, 2001, 3001, 4001, 5001, 6001, 7001, 8001, 9001},
+    };
+
     // _traverseNestedVector(usr_input);
     // _checkingRow(usr_input);
-    _checkingCol(usr_input);
+    // _checkingCol(usr_input);
+    _matrixRowColGenerator(usr_input);
+
+    // TestFunction();
     return 0;
 }
