a0a7461360429984d77cc7df865f01c7c7368519
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-23 14:50:19 +0530

    Checking Boiler Plate Pannel

--- longestConsecutive - hashSet.cpp
+++ longestConsecutive - hashSet.cpp
@@ -12,33 +12,70 @@ using namespace std;
 #include <set>
 #include <unordered_set>
 #include <queue>
-// -- additional_libraries -- azur3 
+// -- additional_libraries -- azur3
 #include "TimerClock.h"
 
-    // Classes
-    
-    // Functions 
+// Classes
 
-    void _showSet(set<int> &nums){
-        cout << "Printing Set " << endl;
-        for(int it : nums){
-            cout << it << " ";
-        }
-        cout << endl;
+// Functions
+
+void _showSet(set<int> &nums)
+{
+    cout << "Printing Set " << endl;
+    for (int it : nums)
+    {
+        cout << it << " \t";
     }
+    cout << endl;
+}
 
+void longestConsecutive(set<int> &nums)
+{
+
+    // int prevElement = -192;
+    int nextElement = 192;
+    int masterCounter = 0;
+    auto endElementIter = nums.end();
+    for (int it : nums )
+    {
+
+        // prevElement = it - 1;
+        nextElement = it - 1;
+        // cout << prevElement << " \t";
+
+        if (nums.find(nextElement) != nums.end())
+        {
+            masterCounter++;
+            // nums.erase(it);
+        }
+        else
+        {
+            cout << "Else Condition :: \n";
+            masterCounter = 0;
+        }
+
+        // else {
+        //     masterCounter = 0;
+        // }
+
+        // _showSet(nums);
+        cout << "master counter : " << masterCounter + 1 << endl;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
 
-    // -- faster I/O operations 
+    // -- faster I/O operations
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     // -- Main Function code --
 
     set<int> s1 = {9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6};
-    _showSet(s1);
+    // _showSet(s1);
+    cout << endl;
+    longestConsecutive(s1);
 
     return 0;
 }
