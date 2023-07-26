c5a738e72c6b6ff61d3d471b465a992423e5f5fa
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-26 21:12:32 +0530

    Starting Trapping Rain Water - neetCode

---
+++ trappingRainWater.cpp
@@ -0,0 +1,54 @@
+// Problem desc 
+
+// Example 1:
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+
+
+// Explanation: The above elevation map (black section) is 
+// represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case,
+// 6 units of rain water (blue section) are being trapped.
+
+// Example 2:
+
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+
+
+// -- essential_libraries_and_namespace
+#include <iostream>
+using namespace std;
+
+// -- additional_libraries
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+// -- additional_libraries -- azur3 
+#include "TimerClock.h"
+
+    // Classes
+    
+    // Functions
+
+
+int main(int argc, char const *argv[])
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+    // -- faster I/O operations 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // -- Main Function code --
+
+
+    return 0;
+}
