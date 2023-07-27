8589694e8f33f4182c7d2acddbbe399161dfe069
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-28 02:18:49 +0530

    Implementing Stacks

--- stack_validParentheses Try - 2.cpp
+++ stack_validParentheses Try - 2.cpp
@@ -21,6 +21,54 @@ using namespace std;
 
 // Functions
 
+template <typename t>
+void _showStack(const stack<t> &stac)
+{
+    cout << "-- -- -- -- -- -- " << endl;
+    cout << "printing Stack " << endl;
+    cout << "-- -- -- -- -- -- " << endl;
+
+    stack<t> tempStack = stac;
+
+    while (!tempStack.empty())
+    {
+        cout << tempStack.top() << endl;
+        tempStack.pop();
+    }
+
+    cout << "-- -- -- -- -- -- " << endl;
+    cout << "Stack printed " << endl;
+    cout << "-- -- -- -- -- -- " << endl;
+}
+
+void ValidParenthesis(string &s)
+{
+    stack<char> s1;
+    for (char letter : s)
+    {
+
+        if ((letter == '[') || (letter == '{') || (letter == '('))
+        {
+            cout << "Pushing " << endl;
+            s1.push(letter);
+        }
+        else
+        {
+            if (s1.empty() ||
+                (letter == ']' && s1.top() != '[') ||
+                (letter == ')' && s1.top() != '(') ||
+                (letter == '}' && s1.top() != '{'))
+
+            {
+                cout << "Error " << endl;
+                return;
+            }
+            s1.pop();
+        }
+    }
+    cout << ":: Correct String :: " << endl;
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -33,6 +81,19 @@ int main(int argc, char const *argv[])
     cin.tie(NULL);
 
     // -- Main Function code --
+
+#if 0
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    _showStack(s);
+#endif
+
+    string s;
+    cin >> s;
+    ValidParenthesis(s);
 
     return 0;
 }
