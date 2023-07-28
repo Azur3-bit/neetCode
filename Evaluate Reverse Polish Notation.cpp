9c801baab2d6f519fa790e8cca6b08e3dbf01bb0
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-28 21:43:40 +0530

    Solved RPN

--- Evaluate Reverse Polish Notation.cpp
+++ Evaluate Reverse Polish Notation.cpp
@@ -1,4 +1,4 @@
-// Evaluate Reverse Polish Notation  
+// Evaluate Reverse Polish Notation
 
 // -- essential_libraries_and_namespace
 #include <iostream>
@@ -15,28 +15,174 @@ using namespace std;
 #include <set>
 #include <unordered_set>
 #include <queue>
-// -- additional_libraries -- azur3 
+// -- additional_libraries -- azur3
 #include "TimerClock.h"
 
-    // Classes
-    
-    // Functions
+#define testCase 3
 
+// Classes
+
+// Functions
+
+template <typename type_1>
+void _input(vector<type_1> &nums)
+{
+
+    // take a single line for input
+    // traverse it and push all the elements into the vector of stirng
+
+    while (testCase--)
+    {
+        string str;
+        getline(cin, str);
+
+        cout << "current Input line " << str << endl;
+
+        for (auto it : str)
+        {
+            nums.push_back(it);
+        }
+
+        cout << "string has been conve"
+    }
+
+#if 0
+    type_1 input;
+    while (cin >> input)
+    {
+        nums.push_back(input);
+    }
+    cout << endl;
+#endif
+}
+
+template <typename type_2>
+void _showVector(const vector<type_2> &nums)
+{
+    cout << ":: Printing Vector :: " << endl;
+    cout << "{ ";
+    for (type_2 it : nums)
+    {
+        cout << it << " ";
+    }
+    cout << "}";
+    cout << endl;
+    cout << ":: Vector Printed :: " << endl;
+}
+
+void selfhelpFuntion()
+{
+
+    char add = '+';
+    char sub = '-';
+    char div = '/';
+    char mul = '*';
+    int addAscii = add;
+    int subAscii = sub;
+    int divAscii = div;
+    int mulAscii = mul;
+    cout << add << " - > " << addAscii << endl;
+    cout << sub << " - > " << subAscii << endl;
+    cout << mul << " - > " << mulAscii << endl;
+    cout << div << " - > " << divAscii << endl;
+}
+
+void RevPolishNotiaion(vector<string> &tokens)
+{
+
+    // stacks for getting all the elements together
+    stack<string> RPN{};
+    // pushing valid values in the stacks
+    for (string it : tokens)
+    {
+        if ((it == "*") || (it == "+") || (it == "/") || (it == "-"))
+        {
+            string op_2 = RPN.top();
+            int intOP_2 = stoi(op_2);
+            RPN.pop();
+            string op_1 = RPN.top();
+            int intOP_1 = stoi(op_1);
+            RPN.pop();
+
+            if (it == "*")
+            {
+                int result = intOP_1 * intOP_2;
+
+                RPN.push(to_string(result));
+            }
+
+            if (it == "+")
+            {
+                int result = intOP_1 + intOP_2;
+                RPN.push(to_string(result));
+            }
+
+            if (it == "-")
+            {
+                int result = intOP_1 - intOP_2;
+                RPN.push(to_string(result));
+            }
+
+            if (it == "/")
+            {
+                int result = intOP_1 / intOP_2;
+                if (result <= 0)
+                {
+                    RPN.push("0");
+                }
+                else
+                {
+                    // string strResult = ;
+                    RPN.push(to_string(result));
+                }
+            }
+        }
+
+        else
+        {
+            RPN.push(it);
+        }
+    }
+    cout << "result : " << stoi(RPN.top()) << endl;
+}
 
 int main(int argc, char const *argv[])
 {
 
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
-    // -- faster I/O operations 
+    // -- faster I/O operations
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     // -- Main Function code --
 
+    // run for multiple inputs
+    // int testCase = 3;
+    int test = testCase;
+    while (test--)
+    {
+        vector<string> tokens{};
+        _input(tokens);
+        _showVector(tokens);
+        RevPolishNotiaion(tokens);
+    }
+
+#if 0
+    // checking string comparions
+    string s = "*";
+    string q = "*";
+    if (s == "*")
+    {
+        cout << "They both are Equal " << endl;
+    }
+    else
+    {
+        cout << "They both are not - Equal " << endl;
+    }
+#endif
 
     return 0;
 }
- 
