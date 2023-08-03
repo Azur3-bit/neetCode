34e2627a724dbe9806c416d090baf0f39bec3fbc
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-08-04 02:37:54 +0530

    Case - 1,2 Passed largest rec Histogram

--- Largest Rectangle in Histogram.cpp
+++ Largest Rectangle in Histogram.cpp
@@ -22,6 +22,88 @@ using namespace std;
 
 // Functions
 
+template <typename type_1>
+void _input(vector<type_1> &nums)
+{
+    type_1 input;
+    while (cin >> input)
+    {
+        nums.push_back(input);
+        if (cin.peek() == '\n')
+            break;
+    }
+    cout << endl;
+}
+
+template <typename type_2>
+void _showVector(const vector<type_2> &nums)
+{
+    cout << ":: Printing Vector :: " << endl;
+    cout << "{ ";
+    for (type_2 it : nums)
+    {
+        cout << it << " ";
+    }
+    cout << "}";
+    cout << endl;
+    cout << ":: Vector Printed :: " << endl;
+}
+
+void largestRectangleArea()
+{
+
+    /* // ? Algorithm -- complex and requires more memory -- must 
+
+        traverse for(i,n)
+        if(current > top && stack.size() == 1)
+            push current
+        else
+            empty stack
+            push element
+
+        width = size
+        final answer = min(top1 , top2) // stack elments only two       elements
+        final answer  = final answer * width
+    */
+}
+
+void largestRectangleAreatry_1(vector<int> &heights)
+{
+
+    /*
+     *algo - 2 -- simple and optimized -- excuted -- heap overflow
+        find max of the array
+        check if right is bigger or left is bigger of the max element
+        return the greater area
+
+    */
+    cout << "working " << endl;
+    _showVector(heights);
+    int left = 0, right = heights[1], forMax = heights[0];
+    for (int i = 0; i < heights.size() - 1; i++)
+    {
+        if (heights[i] > heights[i - 1])
+        {
+            forMax = heights[i];
+            right = heights[i + 1];
+            left = heights[i - 1];
+        }
+    }
+    cout << "for max element : " << forMax << endl;
+    cout << "for max element - right: " << right << endl;
+    cout << "for max element - left : " << left << endl;
+
+    int count = max(right, left);
+
+    int answer = min(count, forMax);
+
+    cout << "count : " << count << endl;
+    cout << "answer : " << answer << endl;
+    count = answer * 2;
+    cout << "final Answer : " << answer * 2 << endl;
+    // return count * 2;
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -34,6 +116,12 @@ int main(int argc, char const *argv[])
     cin.tie(NULL);
 
     // -- Main Function code --
+
+    vector<int> nums;
+    _input(nums);
+
+    _showVector(nums);
+    largestRectangleAreatry_1(nums);
 
     return 0;
 }
