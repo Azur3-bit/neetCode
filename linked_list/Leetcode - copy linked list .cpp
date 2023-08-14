f9b96bb8722fbe1f0d8deb221b5a523da461abe9
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-08-14 19:54:56 +0530

    Created random Linked list

--- linked_list/Leetcode - copy linked list .cpp
+++ linked_list/Leetcode - copy linked list .cpp
@@ -56,7 +56,21 @@ void _showLinkedList(Node *head) {
 	}
 	cout << "null\n";
 }
-// -- vector to link list
+
+// Modified link list Traversal
+void Modi_showLinkedList(Node *head) {
+	cout << "printing Link list \n";
+	while (head != nullptr) {
+		cout << " --- --- --- ---- --- --- --- \n";
+		cout << "head.val : " << head->val << endl;
+		cout << "head.random : " << head->random << endl;
+		// cout << head->random << endl;
+		head = head->next ;
+	}
+	cout << "--- --- --- ---- --- --- ---> \n";
+	cout << "null\n";
+}
+// vector to link list
 template<typename type_1>
 void vec_linkedlist(Node *&head, vector<type_1> &nums) {
 	for (auto it : nums)
@@ -91,20 +105,20 @@ void _showVector(const vector<type_2> &nums)
 	cout << ":: Vector Printed :: " << endl;
 }
 // show hashMap
-template <typename KeyType, typename ValueType>
-void _showHashMap(const std::map<KeyType, ValueType> &hashMap)
+// template <typename KeyType, typename ValueType>
+void _showHashMap(std::map<int, Node *> &hashMap)
 {
 	std::cout << std::endl;
 	std::cout << "-*-  -*- -*- " << std::endl;
 	std::cout << "Printing HashMap" << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "key\t\tvalue" << std::endl;
-	std::cout << "----\t-----" << std::endl;
+	std::cout << "Value_i\t\tMem_add" << std::endl;
+	std::cout << "------\t\t-------" << std::endl;
 
 	for (const auto &it : hashMap)
 	{
-		std::cout << it.first << "\t\t" << it.second << std::endl;
+		std::cout << it.first << "\t\t\t" << it.second << std::endl;
 	}
 
 	std::cout << std::endl;
@@ -112,6 +126,30 @@ void _showHashMap(const std::map<KeyType, ValueType> &hashMap)
 	std::cout << "-*-  -*- -*- " << std::endl;
 	std::cout << std::endl;
 }
+// create hashMap to store all the index node
+map<int, Node *> gen_hashMap(Node*head) {
+	map<int, Node *> hashMap{};
+	int c = 0;
+	while (head != nullptr) {
+		hashMap.insert(make_pair(c, head));
+		head = head->next;
+		c++;
+	}
+	cout << "inserting Node* of nullptr  \n";
+	hashMap.insert(make_pair(c, head));
+	return hashMap;
+}
+// link random pointer to Node * of HashMap
+void LinkingRandom_pointer(Node *&head, vector<int> r, map<int, Node*> &hashMap) {
+	Node *temp = head;
+	for (auto it : r) {
+		if (hashMap.find(it) != hashMap.end()) {
+			temp->random = hashMap[it];
+			temp = temp->next;
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 #ifndef ONLINE_JUDGE
@@ -125,7 +163,7 @@ int main(int argc, char const *argv[])
 
 	// -- Main Function code --
 
-	// -- get both the vectors :- v , r
+	// get both the vectors :- v , r
 
 	cout << "Working !" << endl;
 	vector<int> v{};
@@ -141,7 +179,18 @@ int main(int argc, char const *argv[])
 
 	_showLinkedList(head);
 
+	//checking current location of head
+	cout << "head.val : " << head->val << endl;
+	map<int, Node *> hashMap = gen_hashMap(head);
+	_showHashMap(hashMap);
 
+
+// print Modified Linked list
+	Modi_showLinkedList(head);
+// linking random POinter
+	LinkingRandom_pointer(head, r, hashMap);
+// print Modified Linked list
+	Modi_showLinkedList(head);
 
 	return 0;
 }
