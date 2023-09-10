98dfe710429309b87c349933e7f2fe71dfd1dc50
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-09-10 05:48:39 +0530

    Solving Merge k Sorted Lists

--- linked_list/Merge k Sorted Lists _ Merge Sort.cpp
+++ linked_list/Merge k Sorted Lists _ Merge Sort.cpp
@@ -58,10 +58,31 @@ ostream& operator<<(ostream& os, const pair<T, S>& p) {
 	os << "(" << p.first << ", " << p.second << ")";
 	return os;
 }
+// structure of linked list
+struct ListNode {
+	int val;
+	ListNode * next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+// printing Linked list
+void _showLinkedList(ListNode * head) {
+	while (head) {
+		cout << head->val << " -> ";
+		head = head->next;
+	}
+	cout << "end\n";
+}
+void _showLinkedList_using_vector(vector<ListNode *> nums) {
+	for (auto it : nums)
+		_showLinkedList(it);
+}
 // ------------------------------------------------------------------
-void solve() {
+// ListNode *solve(vector<ListNode *> &nums) {
+void solve(vector<ListNode *> &nums) {
 	// Main code goes here
 	cout << "merge sort K sorted Linked lists \n";
+	_showLinkedList_using_vector(nums);
+
 }
 // ------------------------------------------------------------------
 int main(int argc, char const* argv[]) {
@@ -75,6 +96,36 @@ int main(int argc, char const* argv[]) {
 	// Main function code here
 	int t = 1;
 	// cin >> t;
-	while (t--) {solve();}
+	// create all the linked list Nodes
+	ListNode * first = new ListNode(1);
+	ListNode * forth = new ListNode(4);
+	ListNode * fifth = new ListNode(5);
+	// linking
+	first->next = forth;
+	forth->next = fifth;
+	fifth->next = nullptr;
+
+	ListNode * first_1 = new ListNode(1);
+	ListNode * third_1 = new ListNode(3);
+	ListNode * forth_1 = new ListNode(4);
+
+	// linking
+	first_1->next = third_1;
+	third_1->next = forth_1;
+	forth_1->next = nullptr;
+
+	ListNode * second_2 = new ListNode(2);
+	ListNode * sixth_2 = new ListNode(6);
+	// linking
+	second_2->next = sixth_2;
+	sixth_2->next = nullptr;
+
+	// linking heads
+	ListNode * head_1 = first;
+	ListNode * head_2 = first_1;
+	ListNode * head_3 = second_2;
+
+	vector<ListNode *> nums = {first, first_1, second_2};
+	while (t--) {solve(nums);}
 	return 0;
 }
