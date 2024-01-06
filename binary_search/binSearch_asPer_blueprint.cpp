f5120955ef6c195af3494eca1683e84ac1449fec
Azur3-bit <sharma.sunil12527@gmail.com>
2024-01-06 23:00:48 +0530

    creating blueprint-binarySeach

---
+++ binary_search/binSearch_asPer_blueprint.cpp
@@ -0,0 +1,19 @@
+// binSearch_asPer_blueprint
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int low = 0;
+		int high = nums.size() - 1;
+
+		while (high - low > 1) {
+			int mid = low + (high - low) / 2;
+
+			if (nums[mid] <= target) low = mid;
+			else high = mid - 1;
+		}
+
+		if (nums[low] == target) return low;
+		else if (nums[high] == target) return high;
+		return -1;
+	}
+};
