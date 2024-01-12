ec2bd6f6f23c91e9901708cab6a3e91afda1705c
Azur3-bit <sharma.sunil12527@gmail.com>
2024-01-12 23:58:46 +0530

    done with linearMatrix and binaryArray

--- binary_search/linearMatrix_binaryArrayRetry.cpp
+++ binary_search/linearMatrix_binaryArrayRetry.cpp
@@ -35,8 +35,58 @@ void AddNode_end(ListNode *&head, int value) {ListNode *newNode = new ListNode(v
 void _showLinkedList(ListNode *head) {while (head != NULL) {cout << head->val << " -> "; head = head->next;} cout << "NULL" << endl;}
 void vec_linkedlist(ListNode*&head, vector<int> nums ) {for (auto it : nums) {AddNode_end(head, it);}}
 // ------------------------------------------------------------------ solve
+bool binarySearch(vector<int> &nums, int target) {
+	dbg(nums);
+	int l = 0;
+	int r = nums.size() - 1;
+	while (r - l > 1) {
+		int mid = l + (r - l) / 2;
+		if (nums[mid] < target)
+			l = mid + 1;
+		else
+			r = mid;
+	}
+	// dbg(l);
+	if (nums[l] == target) {
+		cout << "element found at index : " << l << "\n";
+		return true;
+	}
+	if (nums[r] == target) {
+		cout << "element found at index : " << r << "\n";
+		return true;
+	}
+	return false;
+}
+bool matrix(vector<vector<int>> &nums, int target) {
+	int last_element_index = nums[0].size() - 1;
+	for (int i = 0; i < nums.size(); i++) {
+		dbg(nums[i][0]);
+		dbg(nums[i][nums.size()]);
+		if (target >= nums[i][0] && target <= nums[i][last_element_index]) {
+			// if (target >= nums[i][0] && target >= nums[i][nums[0].size()]) {
+			cout << "range found in the vector : " << i << "\n";
+			dbg(nums[i]);
+			int ans = binarySearch(nums[i], target);
+			return ans;
+		}
+		cout << "----------------\n";
+	}
+	return false;
+}
 void solve() {
-	cout << "working \n";
+	int target = 3;
+	// cout << "working \n";
+	vector<vector<int>> nums = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+	dbg(nums);
+	dbg(target);
+	// testing binary Seach
+	// vector<int> test = {1, 3, 5, 7};
+	// dbg(test);
+	int ans = matrix(nums, target);
+	if (ans)
+		cout << "*** main *** -- element found\n";
+	else
+		cout << "*** main *** -- element NOT found\n";
 }
 // ------------------------------------------------------------------ main
 int main(int argc, char const* argv[]) {
