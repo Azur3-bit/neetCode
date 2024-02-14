6c8275ba6c2a85056d9038f9965aee3b27dd9be2
Azur3-bit <sharma.sunil12527@gmail.com>
2024-02-14 19:15:43 +0530

    testing lower and upper bound

---
+++ binary_search/MinimumInRotatedSortedArray.cpp
@@ -0,0 +1,80 @@
+// MinimumInRotatedSortedArray
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+// #include "TimerClock.h" // For Performance Monitor
+using namespace std;
+#define dbg(x) cout <<  #x  << " : " << x << "\n";
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {os << "["; for (int i = 0; i < v.size(); ++i) {os << v[i]; if (i != v.size() - 1)os << ", ";} os << "]"; return os;}
+template <typename T>
+istream& operator>>(istream& is, vector<T>& vec) {T element; while (is >> element) {vec.push_back(element);} return is;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const map<T, S>& m) {cout << "---- Printing Ordered - Map\nKEY\tVALUE\n"; for (const auto& it : m)os << it.first << "\t" << it.second << "\n"; return os;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const unordered_map<T, S>& m) {cout << "---- Printing Unordered - Map\nKEY\tVALUE\n"; for (const auto& it : m)os << it.first << "\t" << it.second << "\n"; return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const set<T>& s) {os << "["; for (auto it = s.begin(); it != s.end(); ++it) {os << *it; if (next(it) != s.end())os << ", ";} os << "]"; return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const unordered_set<T>& s) {os << "["; for (auto it = s.begin(); it != s.end(); ++it) {os << *it; if (next(it) != s.end())os << ", ";} os << "]"; return os;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const pair<T, S>& p) {os << "(" << p.first << ", " << p.second << ")"; return os;}
+struct ListNode {int val; ListNode * next; ListNode (int _val) : val(_val), next(nullptr) {}};
+void AddNode_end(ListNode *&head, int value) {ListNode *newNode = new ListNode(value); if (head == NULL) {head = newNode; return;} ListNode *temp = head; while (temp->next != NULL) {temp = temp->next;} temp->next = newNode;}
+void _showLinkedList(ListNode *head) {while (head != NULL) {cout << head->val << " -> "; head = head->next;} cout << "NULL" << endl;}
+void vec_linkedlist(ListNode*&head, vector<int> nums ) {for (auto it : nums) {AddNode_end(head, it);}}
+// ------------------------------------------------------------------ solve
+int answer(vector<int> nums) {
+	dbg(nums);
+	int r = 0, l = nums.size() - 1;
+	dbg(r);
+	dbg(l);
+	int a = INT_MAX;
+	while (l - r > 1) {
+		int mid = r + (l - r) / 2;
+		if (nums[0] < nums[mid]) {
+			cout << "if condition \n";
+			a = min(nums[0], a);
+			r = mid;
+		}
+		else {
+			cout << "else condition \n";
+			a = min(nums[mid], a);
+			l = mid;
+		}
+	}
+	// dbg(a);
+	return a;
+
+}
+void solve() {
+	// vector<int> nums = {3, 4, 5, 1, 2};
+	// vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+	vector<int> nums = {11, 13, 15, 17};
+	int ans = answer (nums);
+	dbg(ans);
+}
+// ------------------------------------------------------------------ main
+int main(int argc, char const* argv[]) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 1;
+	// cin >> t;
+	while (t--) {solve();}
+	return 0;
+}
---
+++ binary_search/binSeach-Recursive.cpp
@@ -0,0 +1,78 @@
+// binSeach - Recursive
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+// #include "TimerClock.h" // For Performance Monitor
+using namespace std;
+#define dbg(x) cout <<  #x  << " : " << x << "\n";
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {os << "["; for (int i = 0; i < v.size(); ++i) {os << v[i]; if (i != v.size() - 1)os << ", ";} os << "]"; return os;}
+template <typename T>
+istream& operator>>(istream& is, vector<T>& vec) {T element; while (is >> element) {vec.push_back(element);} return is;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const map<T, S>& m) {cout << "---- Printing Ordered - Map\nKEY\tVALUE\n"; for (const auto& it : m)os << it.first << "\t" << it.second << "\n"; return os;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const unordered_map<T, S>& m) {cout << "---- Printing Unordered - Map\nKEY\tVALUE\n"; for (const auto& it : m)os << it.first << "\t" << it.second << "\n"; return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const set<T>& s) {os << "["; for (auto it = s.begin(); it != s.end(); ++it) {os << *it; if (next(it) != s.end())os << ", ";} os << "]"; return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const unordered_set<T>& s) {os << "["; for (auto it = s.begin(); it != s.end(); ++it) {os << *it; if (next(it) != s.end())os << ", ";} os << "]"; return os;}
+template <typename T, typename S>
+ostream& operator<<(ostream& os, const pair<T, S>& p) {os << "(" << p.first << ", " << p.second << ")"; return os;}
+struct ListNode {int val; ListNode * next; ListNode (int _val) : val(_val), next(nullptr) {}};
+void AddNode_end(ListNode *&head, int value) {ListNode *newNode = new ListNode(value); if (head == NULL) {head = newNode; return;} ListNode *temp = head; while (temp->next != NULL) {temp = temp->next;} temp->next = newNode;}
+void _showLinkedList(ListNode *head) {while (head != NULL) {cout << head->val << " -> "; head = head->next;} cout << "NULL" << endl;}
+void vec_linkedlist(ListNode*&head, vector<int> nums ) {for (auto it : nums) {AddNode_end(head, it);}}
+// ------------------------------------------------------------------ solve
+bool binSeach(vector<int> nums, int target, int start, int end) {
+	if (nums[end] < target || nums[start] > target) {
+		return false;
+	}
+	int min = start;
+	int max = end;
+	int mid = start + (end - start) / 2;
+	if (nums[mid ] == target) {
+		return true;
+	}
+	if (nums[mid] < target)
+		return (nums, target, mid, max);
+	else
+		return (nums, target, min, mid);
+}
+void solve() {
+	// cout << "working\n";
+
+	vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8};
+	dbg(nums);
+	int target = -100;
+	dbg(target);
+	bool ans = binSeach(nums, target, 0, nums.size() - 1);
+	if (ans)
+		cout << "found \n";
+	else
+		cout << "NOT found \n";
+}
+// ------------------------------------------------------------------ main
+int main(int argc, char const* argv[]) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 1;
+	// cin >> t;
+	while (t--) {solve();}
+	return 0;
+}
